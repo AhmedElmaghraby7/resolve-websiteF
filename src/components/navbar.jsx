c42c391a9e99416c3d06fd1dbefe40dd1c441d6e
Ahmed Elmaghraby <ahmedelmaghraby113@gmail.com>
2025-07-06 20:11:51 +0300

    f0

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { AnimatePresence, motion } from 'framer-motion';
 function Navbar() {
-    const ClassName =`duration-300 hover:scale-125`
+  const [menu, setMenu] = useState(false);
+  const linkClass = `duration-300 hover:scale-125`;
+
   return (
-    <header className="bg-blue-600 text-white p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold mr-10">Resolve</h1>
+    <header className="bg-blue-600 text-white p-4 shadow-md relative">
+      <div className="container mx-auto flex justify-between items-center ">
+        <h1 className="text-2xl font-bold">Resolve</h1>
         <nav>
-          <ul className="flex space-x-6 flex-wrap pr-5">
-            <li className={ClassName}><Link to="/" >Home</Link></li>
-            <li className={ClassName}><Link to="/about" >About</Link></li>
-            <li className={ClassName}><Link to="/products">Products</Link></li>
-            <li className={ClassName}><Link to="/uploadImage">Upload</Link></li>
-            <li className={ClassName}><Link to="/gallery">Gallery</Link></li>
-            <li className={ClassName}><Link to="/privacy" >Privacy</Link></li>
-          </ul>
+          <AnimatePresence>
+          <motion.ul
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 30 }}
+              transition={{ duration: 0.5,}}
+            className={`${
+              menu ? "flex absolute top-7.5 bg-blue-600 p-5 w-full left-0 items-center" : "hidden"
+            } md:flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 mt-4 md:mt-0`}
+          >
+            <li className={linkClass}><Link onClick={() => setMenu(false)} to="/">Home</Link></li>
+            <li className={linkClass}><Link onClick={() => setMenu(false)} to="/about">About</Link></li>
+            <li className={linkClass}><Link onClick={() => setMenu(false)} to="/products">Products</Link></li>
+            <li className={linkClass}><Link onClick={() => setMenu(false)} to="/uploadImage">Upload</Link></li>
+            <li className={linkClass}><Link onClick={() => setMenu(false)} to="/gallery">Gallery</Link></li>
+            <li className={linkClass}><Link onClick={() => setMenu(false)} to="/privacy">Privacy</Link></li>
+          </motion.ul>
+          </AnimatePresence>
+                  <button
+          className="md:hidden px-3 py-1 rounded "
+          onClick={() => setMenu(!menu)}
+        >
+          ☰
+        </button>
         </nav>
       </div>
     </header>
